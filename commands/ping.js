b9072df76fde251cacc2ce4dec27c2133eace1e3
anemone <103523202+a-nemone@users.noreply.github.com>
2022-04-12 21:20:59 +0100

    beautified

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: "ping",
     access: "ingame",
-    execute(message, uuid, client){
-        if (!client.players[uuid]) {client.chat("You aren't ingame!");return}
+    execute(message, uuid, client) {
+        if (!client.players[uuid]) {
+            client.chat("You aren't ingame!");
+            return
+        }
         client.chat(`Pong! Your ping is ${client.players[uuid].ping}ms!`)
     }
 };
