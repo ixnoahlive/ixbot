df0dced356bb2854d87bd203e9bb24e14c338a88
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-04-13 10:19:12 +0200

    Yo mama so dumb
    She thought JavaScript and Java were the same thing

---
+++ commands/yomama.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const yomama = JSON.parse(fs.readFileSync('../resources/yomama.json'))
+
+module.exports = {
+    name: 'yomama',
+    access: 'public',
+    execute(client, args, uuid) {
+        client.chat(yomama[Math.floor(Math.random() * yomama.length)]);
+    }
+};
