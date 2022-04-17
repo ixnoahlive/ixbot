0db69ebdd20eaf573ac4009c72f85ddf7b8817bc
Luna <90072930+LunaWasFlaggedAgain@users.noreply.github.com>
2022-04-17 08:25:36 -0300

    Clean input in leak.js

--- commands/leak.js
+++ commands/leak.js
@@ -1,9 +1,12 @@
+const { clean } = require('../utils');
+
 const passwords = ["hunter2","12345678","IamABigFemboyThighEnjoyer","FreeRobux","ixNoahIsCool","YouCantHackMe","password","wordpass","markbyronismydaddy","ottothecat","imgayandnobodyknows","erinisanegirl","nocom","qwertyuiop","football","babyyodabestmeme","dreamstansarecool","access","admin","welcome","trustno1","No password"]
+
 module.exports = {
     name: 'leak',
     access: 'public',
     execute(client, args, uuid) {
         if (args.length === 0) return client.chat(`ENTER SOMEONE TO LEAK/DOXX!!!! (UNPATCHED)`);
-        client.chat(`EPIC LEAK [NAME: ${args}, COORDINATES: ${Math.floor(Math.random() * 694201337)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 694201337)}, PASSWORD: ${passwords[Math.floor(Math.random() * passwords.length)]}, IP: ${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}]`)
+        client.chat(`EPIC LEAK [NAME: ${clean(args)}, COORDINATES: ${Math.floor(Math.random() * 694201337)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 694201337)}, PASSWORD: ${passwords[Math.floor(Math.random() * passwords.length)]}, IP: ${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}]`)
     }
 };
