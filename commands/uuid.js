efb74dc97166f5093bbd65cb84ffb7296c3f2af4
Luna <90072930+LunaWasFlaggedAgain@users.noreply.github.com>
2022-04-17 08:27:41 -0300

    Check if the player was found

--- commands/uuid.js
+++ commands/uuid.js
@@ -4,6 +4,7 @@ module.exports = {
     execute(client, args, uuid) {
         if (!args[0]) return client.chat(`/msg ${uuid} Specify a name!`)
         const ixnoahiscool = Object.values(client.players).find(p => p.name === args[0])
+        if (!ixnoahiscool) return client.chat(`/msg ${uuid} Player not found.`)
         client.chat(`/msg ${uuid} ${ixnoahiscool.UUID}`)
     }
 };
