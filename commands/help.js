307f2d70c5c3d53e5f5747213aef3f9e664a97c7
Eva <66324759+eva67x@users.noreply.github.com>
2022-04-19 19:23:57 +0100

    Should fix staff commands appearing for non staff

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-
+const config = require('./config.json');
 module.exports = {
     name: 'help',
     access: 'public',
     execute(client, args, uuid) {
         const commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+         for (var i = 0; i < commands.length; i++) {
+          if(commands[i].access=="staff") {
+              if(!config.staff.includes(uuid)){
+             commands.splice(i, 1); 
+              }
+           }
         client.chat(`Commands: &7${commands.join(', ').replace(/\.js/g, '')}`);
     }
 };
