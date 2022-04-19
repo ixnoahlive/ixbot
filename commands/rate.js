d448c458bd75858c3ae468bd9463a89d64934867
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-04-19 17:49:18 +0200

    Added forced ratings

--- commands/rate.js
+++ commands/rate.js
@@ -6,14 +6,21 @@ module.exports = {
     execute(client, args, uuid) {
 
         let thing2rate;
+        let rating = Math.floor(Math.random() * 6)
 
-        switch (args[0]) {
+        switch (args[0].toLowerCase()) {
+            case 'ixalt':
+            case 'ixbot':
             case 'you':
                 thing2rate = 'myself';
+                rating = 5
                 break;
             case 'me':
                 thing2rate = 'you';
                 break;
+            case 'ixnoah':
+            case 'noah':
+                rating = 5
             default:
                 thing2rate = args.join(' ');
                 break;
@@ -21,6 +28,6 @@ module.exports = {
 
         if (thing2rate.length < 1) return client.chat('Enter something to rate!');
         
-        client.chat(`I rate ${clean(thing2rate)} ${Math.floor(Math.random() * 6)}/5 stars!`);
+        client.chat(`I rate ${clean(thing2rate)} ${rating}/5 stars!`);
     }
 };
